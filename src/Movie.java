4ea13e76b4359cbc9b522103258783bfe6d5c674
Florian Christof <florian@gaia-net.de>
2018-05-20 15:52:08 +0200

    Move getCharge to Movie

--- src/Movie.java
+++ src/Movie.java
@@ -16,5 +16,26 @@ public class Movie {
     }
     public String getTitle (){
         return title;
-    };
+    }
+    
+    double getCharge(int daysRented) {
+    	 double result = 0;
+    	 switch (getPriceCode()) {
+    	 	case Movie.REGULAR:
+    	 		result += 2;
+    	 		if (daysRented > 2)
+    	 			result += (daysRented - 2) * 1.5;
+    	 		break;
+    	 	case Movie.NEW_RELEASE:
+    	 		result += daysRented * 3;
+    	 		break;
+    	 	case Movie.CHILDRENS:
+    	 		result += 1.5;
+    	 		if (daysRented > 3)
+    	 			result += (daysRented - 3) * 1.5;
+    	 		break;
+    	 }
+    	 return result;
+    }
+    
 }
--- src/Rental.java
+++ src/Rental.java
@@ -1,35 +1,19 @@
 class Rental {
-    private Movie movie;
-    private int daysRented;
+    private Movie _movie;
+    private int _daysRented;
     public Rental(Movie newmovie, int newdaysRented) {
-        movie = newmovie;
-        daysRented = newdaysRented;
+        _movie = newmovie;
+        _daysRented = newdaysRented;
     }
     public int getDaysRented() {
-        return daysRented;
+        return _daysRented;
     }
     public Movie getMovie() {
-        return movie;
+        return _movie;
     }
     
     double getCharge() {
-    double result = 0;
-    switch (getMovie().getPriceCode()) {
-    	 case Movie.REGULAR:
-    		 result += 2;
-    	 if (getDaysRented() > 2)
-    		 result += (getDaysRented() - 2) * 1.5;
-    	 	break;
-    	 case Movie.NEW_RELEASE:
-    		 result += getDaysRented() * 3;
-    	 break;
-    	 	case Movie.CHILDRENS:
-    		 result += 1.5;
-    	 	if (getDaysRented() > 3)
-    		 	result += (getDaysRented() - 3) * 1.5;
-    	 	break;
-    	 }
-    	 return result;
+    	return _movie.getCharge(_daysRented);
     }
     
     int getFrequentRenterPoints() {
