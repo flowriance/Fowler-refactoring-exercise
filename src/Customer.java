f792fcbd9aa523d23e03a42db48f168dec21e43a
Florian Christof <florian@gaia-net.de>
2018-05-20 15:30:32 +0200

    change nameing: amountFor()

--- src/Customer.java
+++ src/Customer.java
@@ -41,21 +41,21 @@ class Customer {
         return result;
     }
 
-    private double amountFor(Rental each) {
+    private double amountFor(Rental eRented) {
         double thisAmount = 0;
-        switch (each.getMovie().getPriceCode()) {
+        switch (eRental.getMovie().getPriceCode()) {
             case Movie.REGULAR:
                 thisAmount += 2;
-                if (each.getDaysRented() > 2)
-                    thisAmount += (each.getDaysRented() - 2) * 1.5;
+                if (eRental.getDaysRented() > 2)
+                    thisAmount += (eRental.getDaysRented() - 2) * 1.5;
                 break;
             case Movie.NEW_RELEASE:
-                thisAmount += each.getDaysRented() * 3;
+                thisAmount += eRental.getDaysRented() * 3;
                 break;
             case Movie.CHILDRENS:
                 thisAmount += 1.5;
-                if (each.getDaysRented() > 3)
-                    thisAmount += (each.getDaysRented() - 3) * 1.5;
+                if (eRental.getDaysRented() > 3)
+                    thisAmount += (eRental.getDaysRented() - 3) * 1.5;
                 break;
         }
         return thisAmount;
